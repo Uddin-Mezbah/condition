0d7422095af4b6e2ce6d853f15e4f9dd3e64530d
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-07 14:40:23 +0800

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,30 @@
+"""var1 = 6
+var2 = 56
+
+var3 = int(input())
+
+if var3>var2:
+    print("Greater")
+if var3==var2:
+    print("equal")
+else:
+    print("Leaser")
+list = [2,4,5]
+if 4 in list:
+    print("yes in the list")
+else:
+    print("no in the list") """
+
+age = int(input())
+
+if age < 18:
+    print("you can not watching adult movie")
+if age  > 18:
+    print("you can drive")
+else:
+    print()
+
+
+
+
+
